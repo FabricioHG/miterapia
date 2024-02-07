527f7cf9106128f6ca799a9e1928856b571fc5f9
Fabricio Hernandez <fabriciofranciscohernandezgome@gmail.com>
2024-02-07 15:15:50 -0600

    Cambio de rama para hacer merge

---
+++ web/sites/default/files/php/twig/65c3ed6944363_slide_autoplay.html.twig_qPZ6wzf4SgQY4S7E79-sMuAOg/ecyGpovgGlZr8ilcufvv4keRfCWcDcgwMUw68gwTpvc.php
@@ -0,0 +1,163 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/custom/webs_systems/templates/slide_autoplay.html.twig */
+class __TwigTemplate_05698608f26ed416768e68c40ef82f72 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_ad96c2d8979d8d23860453e7c5eb1520 = $this->extensions["Drupal\\tracer\\Twig\\Extension\\TraceableProfilerExtension"];
+        $__internal_ad96c2d8979d8d23860453e7c5eb1520->enter($__internal_ad96c2d8979d8d23860453e7c5eb1520_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "modules/custom/webs_systems/templates/slide_autoplay.html.twig"));
+
+        // line 1
+        echo "
+";
+        // line 6
+        echo "
+<div class=\"container\">
+\t<div class=\"autoplay\">
+\t";
+        // line 9
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["url_imgs"] ?? null));
+        foreach ($context['_seq'] as $context["_key"] => $context["url_img"]) {
+            // line 10
+            echo "\t\t";
+            if ($context["url_img"]) {
+                // line 11
+                echo "\t  \t\t";
+                // line 12
+                echo "\t  \t\t<div class=\"slide slide_car_auto_play\">
+\t\t    \t<img src=\"";
+                // line 13
+                echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, Drupal\twig_tweak\TwigTweakExtension::imageStyleFilter($this->sandbox->ensureToStringAllowed($context["url_img"], 13, $this->source), "carousel_auto_265_x_370"), "html", null, true);
+                echo "\" alt=\"Imagen 1\">
+\t\t  \t</div>
+\t\t";
+            }
+            // line 16
+            echo "\t";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['url_img'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 17
+        echo "\t</div>
+</div>
+
+
+
+";
+        $this->env->getExtension('\Drupal\Core\Template\TwigExtension')
+            ->checkDeprecations($context, ["url_imgs"]);        
+        $__internal_ad96c2d8979d8d23860453e7c5eb1520->leave($__internal_ad96c2d8979d8d23860453e7c5eb1520_prof);
+
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName()
+    {
+        return "modules/custom/webs_systems/templates/slide_autoplay.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo()
+    {
+        return array (  74 => 17,  68 => 16,  62 => 13,  59 => 12,  57 => 11,  54 => 10,  50 => 9,  45 => 6,  42 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("
+{# variables que se pueden utilizar
+\t- url_imgs
+\t- titulo
+#}
+
+<div class=\"container\">
+\t<div class=\"autoplay\">
+\t{% for url_img in url_imgs %}
+\t\t{% if url_img %}
+\t  \t\t{# Renderizar la imagen utilizando el estilo de imagen \"carousel_auto_265_x_370\" #}
+\t  \t\t<div class=\"slide slide_car_auto_play\">
+\t\t    \t<img src=\"{{url_img|image_style('carousel_auto_265_x_370')}}\" alt=\"Imagen 1\">
+\t\t  \t</div>
+\t\t{% endif %}
+\t{% endfor %}
+\t</div>
+</div>
+
+
+
+", "modules/custom/webs_systems/templates/slide_autoplay.html.twig", "/Applications/XAMPP/xamppfiles/htdocs/PlantillaWs/web/modules/custom/webs_systems/templates/slide_autoplay.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("for" => 9, "if" => 10);
+        static $filters = array("escape" => 13, "image_style" => 13);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['for', 'if'],
+                ['escape', 'image_style'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
---
+++ web/sites/default/files/php/twig/65c3ed6944363_time.html.twig_a30DIVGzOzt62RTUkB1uQwo94/-suJoSq6vPj8ZsHlKvGGlgMRQ3BgQB9eQ3IZclVHKVY.php
@@ -0,0 +1,132 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/contrib/bootstrap5/templates/field/time.html.twig */
+class __TwigTemplate_264fb22ed706ad49b073d39ba1e2e916 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_ad96c2d8979d8d23860453e7c5eb1520 = $this->extensions["Drupal\\tracer\\Twig\\Extension\\TraceableProfilerExtension"];
+        $__internal_ad96c2d8979d8d23860453e7c5eb1520->enter($__internal_ad96c2d8979d8d23860453e7c5eb1520_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "themes/contrib/bootstrap5/templates/field/time.html.twig"));
+
+        // line 22
+        echo "<time";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", ["datetime"], "method", false, false, true, 22), 22, $this->source), "html", null, true);
+        echo ">";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["text"] ?? null), 22, $this->source), "html", null, true);
+        echo "</time>
+";
+        $this->env->getExtension('\Drupal\Core\Template\TwigExtension')
+            ->checkDeprecations($context, ["attributes", "text"]);        
+        $__internal_ad96c2d8979d8d23860453e7c5eb1520->leave($__internal_ad96c2d8979d8d23860453e7c5eb1520_prof);
+
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName()
+    {
+        return "themes/contrib/bootstrap5/templates/field/time.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo()
+    {
+        return array (  42 => 22,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{#
+/**
+ * @file
+ * Theme override for a date / time element.
+ *
+ * Available variables
+ * - timestamp: (optional) A UNIX timestamp for the datetime attribute. If the
+ *   datetime cannot be represented as a UNIX timestamp, use a valid datetime
+ *   attribute value in attributes.datetime.
+ * - text: (optional) The content to display within the <time> element.
+ *   Defaults to a human-readable representation of the timestamp value or the
+ *   datetime attribute value using DateFormatter::format().
+ * - attributes: (optional) HTML attributes to apply to the <time> element.
+ *   A datetime attribute in 'attributes' overrides the 'timestamp'. To
+ *   create a valid datetime attribute value from a UNIX timestamp, use
+ *   DateFormatter::format() with one of the predefined 'html_*' formats.
+ *
+ * @see template_preprocess_time()
+ * @see http://www.w3.org/TR/html5-author/the-time-element.html#attr-time-datetime
+ */
+#}
+<time{{ attributes.addClass('datetime') }}>{{ text }}</time>
+", "themes/contrib/bootstrap5/templates/field/time.html.twig", "/Applications/XAMPP/xamppfiles/htdocs/PlantillaWs/web/themes/contrib/bootstrap5/templates/field/time.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 22);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
